c411d6397ba4861eb92f3ef48db9fac7c0c15cd9
“John <johnjamesmartin@live.com>
2017-06-20 21:29:00 +0100

    Refactor and clean layout

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,11 @@
 class RecipesController < ApplicationController
+    before_action :set_recipe, only: [:show, :edit, :update]
+
     def index
         @recipes = Recipe.all
     end
 
     def show
-        @recipe = Recipe.find(params[:id])
     end
 
     def new
@@ -22,37 +23,31 @@ class RecipesController < ApplicationController
         end
     end
 
+    def edit
+    end
 
-def edit
-  @recipe = Recipe.find(params[:id])
-end
+    def update
+    if @recipe.update(recipe_params)
+        flash[:success] = "Recipe was updated successfully!"
+        redirect_to recipe_path(@recipe)
+    else
+        render 'edit'
+    end
+    end
 
-def update
-  @recipe = Recipe.find(params[:id])
-  if @recipe.update(recipe_params)
-    flash[:success] = "Recipe was updated successfully!"
-    redirect_to recipe_path(@recipe)
-  else
-    render 'edit'
-  end
-end
+    def destroy
+        Recipe.find(params[:id]).destroy
+        flash[:success] = "Recipe deleted successfully"
+        redirect_to recipes_path
+    end
 
-def destroy
-    Recipe.find(params[:id]).destroy
-    flash[:success] = "Recipe deleted successfully"
-    redirect_to recipes_path
-end
+    private
 
-
-
-
-
-private
-
-def recipe_params
-    params.require(:recipe).permit(:name, :description)
-end
-
-
+    def set_recipe
+        @recipe = Recipe.find(params[:id])
+    end
+    def recipe_params
+        params.require(:recipe).permit(:name, :description)
+    end
 
 end
